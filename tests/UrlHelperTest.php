babf3d1542e4e2a25fb19a6d0dec7db1278135f3
Tiny.Poro <ngophuongtuan@gmail.com>
2017-11-02 20:16:48 +0700

    update unit test
    update unit test

--- tests/UrlHelperTest.php
+++ tests/UrlHelperTest.php
@@ -10,8 +10,11 @@ class UrlHelperTest extends TestCase
         // test to ensure that the object from an fsockopen is valid
     
         $connObj = new urlHelper();
-        $url = "https://www.w3schools.com.";
+        $url = "https://www.w3schools.com";
         $this->assertTrue($connObj->check($url), "$url is a valid url");
+
+        $url = "https:w.w3schools.com";
+        $this->assertFalse($connObj->check($url), "$url is not a valid url");
     }
 
      public function testConnect()
@@ -20,7 +23,9 @@ class UrlHelperTest extends TestCase
         $connObj = new urlHelper();
         $url = "http://google.com/a/";
         $path = "/xyz.html";
-        $this->assertTrue($connObj->connect($url, $path) == "http://google.com/xyz.html");
+        $this->assertTrue($connObj->connect($url, $path) == "http://google.com/xyz.html", "$url connect $path is http://google.com/xyz.html");
+        $path = "xyz.html";
+        $this->assertTrue($connObj->connect($url, $path) == "http://google.com/a/xyz.html", "$url connect $path is http://google.com/a/xyz.html");
     }
 
      public function testGetIno()
@@ -28,6 +33,9 @@ class UrlHelperTest extends TestCase
         // test to ensure that the object from an fsockopen is valid
         $connObj = new urlHelper();
         $url = "http://google.com/a/";
-        $this->assertTrue($connObj->getInfo($url) == "http");
+        $arr = $connObj->getInfo($url);
+        $this->assertTrue($arr['protocol'] == "http", "protocol of $url is http");
+        // $this->assertTrue($arr['port'] == "http", "port of $url is http");
+        $this->assertTrue($arr['domain'] == "google.com", "domain of $url is google.com");
     }
 }
